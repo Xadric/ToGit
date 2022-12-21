96aa434b9aa3586643bd9b7d2815614df9ca962d
Xadric <artem.prime2008@gmail.com>
2022-12-21 19:11:40 +0200

    added souf

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,13 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        new Main().run();
+        new Main().run1();
+    }
+
+    private void run1() {
+        int k1 = 7;
+        int k2 = 7;
+        System.out.printf("It is %d apples in %d box",k2,k1);
     }
 
     private void run() {
