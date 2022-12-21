6843ca9b3144f743f55d7114ba530b810ce31112
Xadric <artem.prime2008@gmail.com>
2022-12-21 19:03:29 +0200

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        new Main().run();
+    }
+
+    private void run() {
+        System.out.println("Hello world!");
+    }
+}
